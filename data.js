b6edad01a2a2a198019525ef0489abfbf8b2d04e
Step090 <stephanie.moltchanoff@gmail.com>
2025-10-21 13:12:38 +0200

    Creation-Pano-360

---
+++ data.js
@@ -0,0 +1,84 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-insertion-vue-drone-",
+      "name": "Insertion vue drone ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.5104397137278873,
+          "pitch": 0.32744166271052677,
+          "title": "La Réserve",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-visite-360-bordeaux-eterna",
+      "name": "Visite 360 Bordeaux Eterna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
